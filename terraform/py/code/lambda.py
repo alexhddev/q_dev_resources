f7373eee333380898777164d785a14371b770953
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-10-16 16:00:06 +0300

    api gateway

--- terraform/py/code/lambda.py
+++ terraform/py/code/lambda.py
@@ -7,9 +7,27 @@ bucket_name = os.environ['MESSAGES_BUCKET']
 
 # simple handler for aws lambda
 def handler(event, context):
-    print(bucket_name)
+    
+    method = event['httpMethod']
+    # handle get requests
+    if method == 'GET':
+        return get_messages()
+    if method == 'POST':
+        return post_message(event)
+    else:
+        return {
+            'statusCode': 405,
+            'body': 'Method Not Allowed'
+        }
+    
+def get_messages():
     return {
         'statusCode': 200,
-        'body': 'Hello from Lambda!'
+        'body': 'Hello from get'
+    }
+def post_message(event):
+    return {
+        'statusCode': 200,
+        'body': 'Hello from post'
     }
 
