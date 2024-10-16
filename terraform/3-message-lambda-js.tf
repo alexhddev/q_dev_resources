3fc17cd24b9e835c970b2e88d3783bf1388ce64d
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-10-16 19:10:15 +0300

    s3 sdk calls

--- terraform/3-message-lambda-js.tf
+++ terraform/3-message-lambda-js.tf
@@ -30,6 +30,11 @@ resource "aws_iam_role_policy_attachment" "js_message_lambda_policy_attachment" 
     policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+resource "aws_iam_role_policy_attachment" "js_message_lambda_role_policy_attachment" {
+    role = aws_iam_role.js_message_lambda_role.name
+    policy_arn = aws_iam_policy.messages_bucket_access_lambda_policy.arn
+}
+
 resource "aws_lambda_function" "js_message_lambda" {
     function_name = "js-message-lambda"
     role = aws_iam_role.js_message_lambda_role.arn
--- terraform/js/src/HandleMessage.mjs
+++ terraform/js/src/HandleMessage.mjs
@@ -1,15 +1,50 @@
+import { PutObjectCommand, S3Client, ListObjectsV2Command } from "@aws-sdk/client-s3";
 
+const s3Client = new S3Client()
+const messagesBucket = process.env.MESSAGES_BUCKET
 
-const bucketName = process.env.MESSAGE_BUCKET;
-
-// simple aws lambda handler
 async function handler(event, context) {
-  console.log(bucketName)
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
-  };
-  return response;
+
+    try {
+        switch (event.httpMethod) {
+            case 'GET':
+                // get all the files in the bucket
+                const listObjectsCommand = new ListObjectsV2Command({ Bucket: messagesBucket });
+                const response = await s3Client.send(listObjectsCommand);
+    
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify(response.Contents)
+                };
+            case 'POST':
+                if (event.body) {
+                    const command = new PutObjectCommand({
+                        Bucket: process.env.MESSAGES_BUCKET,
+                        Key: `${Date.now()}.json`,
+                        Body: event.body
+                    });
+                    const response = await s3Client.send(command);
+                    return {
+                        statusCode: 200,
+                        body: JSON.stringify({ message: 'File uploaded successfully', response })
+                    };
+                }
+                break;
+    
+            default:
+                return{
+                    statusCode: 405,
+                    body: JSON.stringify({ message: 'Method Not Allowed' })
+                };
+        }
+    } catch (error) {
+        console.error(error);
+    }
+
+    return {
+        statusCode: 500,
+        body: JSON.stringify({ message: 'internal server error' })
+    }    
 }
 
-export { handler };
+export { handler }
